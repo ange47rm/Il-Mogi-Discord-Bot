55a1c4a71b73c3c1ef5459032895e26c2a1f36d6
ange47rm <ange47rm@hotmail.com>
2024-09-27 21:34:11 +0100

    Fixed sounds

--- helpers/soundFunctions.js
+++ helpers/soundFunctions.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const greetAll = (player) => {
-    const resource = createAudioResource(path.join(__dirname, 'assets', "ciaooo.mp3"));
+    const resource = createAudioResource(path.join(__dirname, '../assets', "ciaooo.mp3"));
     player.play(resource);
 }
 
